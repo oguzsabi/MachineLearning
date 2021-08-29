376163f946a3bc328bc0d8c50ed59b16bea21801
OguzSabitay <oguz.sabitay@supplychainwizard.com>
2021-08-29 16:09:53 +0300

    Preprocessing of categorical data for country column and purchased column

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
@@ -11,7 +13,20 @@ y = dataset.iloc[:, -1].values
 
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 X[:, 1:3] = imputer.fit(X[:, 1:3]).transform(X[:, 1:3])
+# print(X[:, 1:3])
 # imputer.fit(X[:, 1:3])
+# print(X[:, 1:3])
 # X[:, 1:3] = imputer.transform(X[:, 1:3])
+# print(X[:, 1:3])
 
-print(X)
+# print(X)
+
+# Encoding categorical data
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X)) # independent values must be numpy array for later use
+# print(X)
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
