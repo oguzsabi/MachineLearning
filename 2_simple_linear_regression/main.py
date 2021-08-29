0b497674f31ff42e84b2e126434251d00c4e50c2
OguzSabitay <oguz.sabitay@supplychainwizard.com>
2021-08-29 22:31:46 +0300

    Implemented simple linear regression

---
+++ 2_simple_linear_regression/main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+# print(X)
+# print(y)
+
+# Splitting the dataset into training set and test set
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# print(X_train)
+# print(X_test)
+# print(y_train)
+# print(y_test)
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_test)
+y_pred_train = lr.predict(X_train)
+# print(y_train - y_pred_train)
+
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, lr.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+plt.scatter(X_test, y_test, color='red')
+plt.scatter(X_test, y_pred, color='orange')
+# plt.plot(X_test, y_pred, color='blue')
+# The regression line is already created and plotting the training set or the test set are the same thing
+plt.plot(X_train, lr.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+print(lr.predict([[12]]))  # Single value prediction
+print(lr.intercept_)  # b0
+print(lr.coef_)  # b1
