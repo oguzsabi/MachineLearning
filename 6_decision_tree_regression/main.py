c15c7308807c69edc761e5e543eaae7d00cf68b7
Oguz Sabitay <oguzsabitay@gmail.com>
2021-09-05 23:49:55 +0300

    Decision tree graph corrections

--- 6_decision_tree_regression/main.py
+++ 6_decision_tree_regression/main.py
@@ -21,7 +21,7 @@ plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
-X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.001)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
 plt.plot(X_grid, dtr.predict(X_grid), color='blue')
