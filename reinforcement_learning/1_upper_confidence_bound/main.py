a379df8d746eb4bbcdea572d7d2c3c8df55d0d44
OguzSabitay <oguz.sabitay@supplychainwizard.com>
2021-09-26 23:58:32 +0300

    Implemented upper confidence bound

---
+++ reinforcement_learning/1_upper_confidence_bound/main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+N = 600
+no_of_ads = 10
+ads_selected = []
+numbers_of_selections = [0] * no_of_ads
+sums_of_rewards = [0] * no_of_ads
+total_reward = 0
+
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+
+    for i in range(0, no_of_ads):
+        if numbers_of_selections[i] > 0:
+            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+            delta_i = math.sqrt(3 / 2 * math.log(n + 1) / numbers_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
+
+    ads_selected.append(ad)
+    numbers_of_selections[ad] += 1
+    reward = dataset.values[n, ad]
+    sums_of_rewards[ad] += reward
+    total_reward += reward
+
+plt.hist(ads_selected)
+plt.title('Ad selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times the ad was selected')
+plt.show()
